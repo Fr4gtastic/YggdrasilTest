d411e301eaa4b6513d0ba05e77a232f84f9b4e3c
Fr4gtastic <milosz.swierad@gmail.com>
2018-06-04 19:36:29 +0200

    Working first version without tests

---
+++ src/main/java/Bonus.java
@@ -0,0 +1,3 @@
+public enum Bonus {
+    GAME_OVER, EXTRA_LIFE, SECOND_CHANCE
+}
---
+++ src/main/java/MainClass.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class MainClass {
+
+    private static int numberOfLoops = (int)1e7;
+    private static ArrayList<Integer> listOfRewards = new ArrayList<>();
+
+    private static double calculateAverage() {
+
+        Integer sum = 0;
+
+        for (Integer integer : listOfRewards) {
+            sum += integer;
+        }
+
+        return sum/listOfRewards.size();
+    }
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < numberOfLoops; i++) {
+            Game game = new Game();
+            game.playGame();
+            listOfRewards.add(game.getTreasury());
+        }
+
+        System.out.println("The average reward after one round, calculated using 10 mln simulations: " + calculateAverage());
+    }
+}
